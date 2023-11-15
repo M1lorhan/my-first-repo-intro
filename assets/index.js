fdd5752f4ef9093736d8d9e409fca2d2f1956099
Marshall Lorhan <mlorhan@gmail.com>
2023-11-14 22:00:36 -0800

    Adjusted HTML.index file to include food truck website

---
+++ assets/index.js
@@ -0,0 +1,6 @@
+$("button").click(function(){
+    $("truckIcon").fadeIn(delay-300);
+    $("favorites").fadeIn(delay-300);
+    $("hours").fadeIn(delay-300);
+    $("Location").fadeIn(delay-300);
+})
